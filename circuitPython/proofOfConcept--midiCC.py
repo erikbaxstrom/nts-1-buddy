281ee87735ef598f6d5afa2a743ba925f8315c19
Erik M Baxstrom <baxstrem@gmail.com>
2022-11-22 14:33:24 -0800

    fix proof of concept b/c save got messed up

--- circuitPython/proofOfConcept--midiCC.py
+++ circuitPython/proofOfConcept--midiCC.py
@@ -6,30 +6,6 @@ import digitalio
 import time
 import busio
 import adafruit_midi
-
-
-led = digitalio.DigitalInOut(board.LED)
-led.direction = digitalio.Direction.OUTPUT
-
-CHANNEL = 1
-
-uart = busio.UART(board.GP0, board.GP1, baudrate=31250, timeout=0.001)
-midi = adafruit_midi.MIDI(midi_out=uart, midi_in=uart, out_channel=CHANNEL, debug=False)
-
-
-while True:
-    led.value = True
-    time.sleep(2)
-    led.value = False
-    time.sleep(1)
-# Pi Pico MIDI controller
-
-
-import board
-import digitalio
-import time
-import busio
-import adafruit_midi
 from adafruit_midi.control_change import ControlChange
 
 
@@ -43,7 +19,6 @@ midi = adafruit_midi.MIDI(midi_out=uart, midi_in=uart, out_channel=CHANNEL -1, d
 
 
 while True:
-    # every 3 seconds, blink LED and change delay time
     midi.send(ControlChange(30, 20))
     led.value = True
     time.sleep(3)
