d5671253ff952b2d2592f2b0ab0046d4d0abfcda
Erik M Baxstrom <baxstrem@gmail.com>
2022-11-22 15:46:39 -0800

    set up digital io and code to confirm I can read the pots and buttons before putting in enclosure

--- circuitPython/mvp.py
+++ circuitPython/mvp.py
@@ -16,8 +16,8 @@ CHANNEL = 1
 uart = busio.UART(board.GP0, board.GP1, baudrate=31250, timeout=0.001)
 midi = adafruit_midi.MIDI(midi_out=uart, midi_in=uart, out_channel=CHANNEL - 1, debug=False)
 
-mux_0 = digitalio.DigitalInOut(board.GP18)  # GP18
-mux_1 = digitalio.DigitalInOut(board.GP19)  # gp19
+mux_0 = digitalio.DigitalInOut(board.GP18)
+mux_1 = digitalio.DigitalInOut(board.GP19)
 mux_2 = digitalio.DigitalInOut(board.GP20)
 
 mux_0.direction = digitalio.Direction.OUTPUT
@@ -29,21 +29,35 @@ mux_1.value = True
 mux_2.value = True
 
 mux_reading = AnalogIn(board.GP28)
-mux_reading.direction = digitalio.Direction.INPUT
 
+button_0 = digitalio.DigitalInOut(board.GP13)
+button_1 = digitalio.DigitalInOut(board.GP12)
+button_2 = digitalio.DigitalInOut(board.GP11)
+button_3 = digitalio.DigitalInOut(board.GP10)
+
+button_0.direction = digitalio.Direction.INPUT
+button_1.direction = digitalio.Direction.INPUT
+button_2.direction = digitalio.Direction.INPUT
+button_3.direction = digitalio.Direction.INPUT
+
+button_0.pull = digitalio.Pull.DOWN
+button_1.pull = digitalio.Pull.DOWN
+button_2.pull = digitalio.Pull.DOWN
+button_3.pull = digitalio.Pull.DOWN
 
 while True:
     # read potentiometer
     # set digital pins
-    mux_0.value = True
-    mux_1.value = True
-    mux_2.value = True
+    mux_0.value = False
+    mux_1.value = False
+    mux_2.value = False
     pot_value = mux_reading.value
-    print('pot_value: ', pot_value)
+    midi_value = int(pot_value/512)
+    print('midi_value: ', midi_value, 'button:', button_0.value, button_1.value, button_2.value, button_3.value)
     # read/store mux_reading
     # wait?
     # repeat
     # output midi
-    # midi.send(ControlChange(30, 20))
-    led.value = not led.value
-    time.sleep(2)
+    midi.send(ControlChange(31, midi_value))
+    #led.value = not led.value
+    time.sleep(0.1)
